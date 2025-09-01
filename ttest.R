87970be043bae8337774391e4f5fbc9943a73421
chelimo <144703887+sandrakemboi@users.noreply.github.com>
2025-09-01 23:21:17 +0300

    Add files via upload

--- ttest.R
+++ ttest.R
@@ -1,9 +1,35 @@
 # import libraries
-library(readxl)
-library(ggplot2)
-library(ggpubr)
-library(dplyr)
-library(car)
-library(effsize)
+library(tidyverse)
 
-#load data
+# load data
+data <-read.csv("C:\\Users\\pc\\Desktop\\DAN\\student-mat.csv")
+view(data)
+
+# Subsetting columns of interest
+
+data_students <-data[,c("sex","G3")]
+view(data_students)
+
+#check missing values and total number of rows
+dim
+
+#check null values
+sum(is.na(data_students))
+
+#
+table(students$sex)
+
+boxplot(G3 ~ sex, data = students, main="Final Grade by Gender", ylab="G3")
+
+
+shapiro.test(students$G3[students$sex == "M"])
+shapiro.test(students$G3[students$sex == 
+                           
+
+library(car)
+leveneTest(G3 ~ sex, data = students)
+
+
+t.test(G3 ~ sex, data = students, var.equal = TRUE)   # Student's t-test
+t.test(G3 ~ sex, data = students)                     # Welch’s t-test (default)
+
